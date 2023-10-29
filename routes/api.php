b9746e11e74117c24c77767ad74c86a60b0d9d5c
badr <badrhamidou@gmail.com>
2023-10-29 15:17:48 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Http\Controllers\ControllerPartida;
+use App\Http\Controllers\ControllerRonda;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerUsuario;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+Route::prefix('admin')->group( function ()  {
+    Route::controller(ControllerUsuario::class)->group( function () {
+        Route::prefix('user')->group(function (){
+            Route::get('/','index');
+            Route::get('/{id}','show');
+            Route::put('/{id}','update');
+            Route::post('/','store');
+            Route::delete('/{id}','destroy');
+        });
+    });
+
+    Route::controller(ControllerPartida::class)->group( function () {
+        Route::prefix('partida')->group(function (){
+            Route::get('/','index');
+            Route::get('/{id}','show');
+            Route::put('/{id}','update');
+            Route::post('/','store');
+            Route::delete('/{id}','destroy');
+        });
+    });
+
+    Route::controller(ControllerRonda::class)->group( function () {
+        Route::prefix('ronda')->group(function (){
+            Route::get('/','index');
+            Route::get('/{id}','show');
+            Route::put('/{id}','update');
+            Route::post('/','store');
+            Route::delete('/{id}','destroy');
+        });
+    });
+});
