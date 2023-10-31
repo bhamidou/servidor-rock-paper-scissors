1c39cdd3d1aa5e9860b4e4b27fd2e791a3793af8
badr <badrhamidou@gmail.com>
2023-10-31 12:33:11 +0100

    create factory

---
+++ database/factories/UsuarioFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Usuario;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Usuario>
+ */
+class UsuarioFactory extends Factory
+{
+    protected $model = Usuario::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->name,
+            'email' => $this->faker->email ,
+            'password' => $this->faker->password(),
+            'pg' => rand(0,100),
+            'pj' => rand(50,100),
+            'rol' => rand(0,5)
+        ];
+    }
+}
