27ce66092dd4efe8af5e9b4a2ebb99809825392a
badr <badrhamidou@gmail.com>
2023-10-31 12:34:10 +0100

    fix column name of hidden

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
-    protected $table = "usuarios";
+    protected $table = "usuario";
     public $timestamps = false;
-    protected $hidden = ['pass'];
+    protected $hidden = ['password'];
 }
